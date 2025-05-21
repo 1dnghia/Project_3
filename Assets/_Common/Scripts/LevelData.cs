3b618931ba4df520056b4cf27223ddf397b9a6db
1dnghia <nghia20031212@gmail.com>
2025-05-21 21:59:39 +0700

    LevelData and LevelList

--- Assets/_Common/Scripts/LevelData.cs
+++ Assets/_Common/Scripts/LevelData.cs
@@ -9,8 +9,31 @@ public class LevelData : ScriptableObject
    public List<Edge> Edgs;
 }
 
-[System.Serializable]
+[System.Serializable]   
 public struct Edge
 {
-   
+    public List<Vector2Int> Points;
+    public Vector2Int StartPoint
+    {
+        get
+        {
+            if(Points != null && Points.Count > 0)
+            {
+                return Points[0];
+            }
+            return new Vector2Int(1, -1);
+        }
+    }
+
+    public Vector2Int EndPoint
+    {
+        get
+        {
+            if (Points != null && Points.Count > 0)
+            {
+                return Points[Points.Count - 1];
+            }
+            return new Vector2Int(-1, -1);
+        }
+    }
 }
--- Assets/_Common/Scripts/LevelList.cs
+++ Assets/_Common/Scripts/LevelList.cs
@@ -2,17 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelList : MonoBehaviour
+[CreateAssetMenu(fileName = "LevelList", menuName = "SO/LevelList")]
+public class LevelList : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public List<LevelData> Levels;
 }
