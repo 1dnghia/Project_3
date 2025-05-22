b3767c5e0211ae8f443f0716d0e9c529c0ecc1f5
1dnghia <nghia20031212@gmail.com>
2025-05-22 21:44:32 +0700

    update GameManager

--- Assets/_Project/Scripts/GameManager.cs
+++ Assets/_Project/Scripts/GameManager.cs
@@ -3,33 +3,110 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-    public class GameManager : NMonoBehaviour
+public class GameManager : NMonoBehaviour
+{
+    [HideInInspector] public int CurrentStage;
+    [HideInInspector] public int CurrentLevel;
+    [HideInInspector] public string StageName;
+
+    [SerializeField] private LevelData DefaultLevel;
+    [SerializeField] private LevelList _allLevels;
+
+    private Dictionary<string, LevelData> Levels;
+
+    private const string MainMenu = "MainMenu";
+    private const string Gameplay = "Gameplay";
+    public static GameManager Instance => _instance;
+    private static GameManager _instance;
+
+    protected override void Awake()
     {
-        [HideInInspector] public int CurrentStage;
-        [HideInInspector] public int CurrentLevel;
-        [HideInInspector] public string StageName;
-        public static GameManager Instance => _instance;
-        private static GameManager _instance;
-
-        protected override void Awake()
+        if (_instance == null)
         {
-            if (_instance == null)
-            {
-                _instance = this;
-                Init();
-                DontDestroyOnLoad(gameObject);
-            }
-            else if (_instance != this)
-            {
-                Destroy(gameObject);
-            }
+            _instance = this;
+            Init();
+            DontDestroyOnLoad(gameObject);
         }
-
-        protected void Init()
+        else if (_instance != this)
         {
-            CurrentStage = 1;
-            CurrentLevel = 1;
-
-                        
+            Destroy(gameObject);
         }
     }
+
+    protected void Init() //khoi tao
+    {
+        CurrentStage = 1;
+        CurrentLevel = 1;
+
+        Levels = new Dictionary<string, LevelData>();// thêm hết tên level từ _allLevels.Levels vào thư viện Levels
+        foreach (var item in _allLevels.Levels)
+        {
+            Levels[item.levelName] = item;
+        }
+    }
+
+    public bool IsLevelUnlocked(int level)// kiểm tra xem một level được mở khóa chưa
+    {
+        string levelName = "Level" + CurrentStage.ToString() + level.ToString();
+        if (level == 1)
+        {
+            PlayerPrefs.SetInt(levelName, 1);
+            return true;
+        }
+
+        if (PlayerPrefs.HasKey(levelName))
+        {
+            return PlayerPrefs.GetInt(levelName) == 1;
+        }
+        PlayerPrefs.SetInt(levelName, 0);
+        return false;
+    }
+
+    public void UnlockLevel()//mở khoá level tiếp theo
+    {
+        CurrentLevel++;
+        if (CurrentLevel == 51)
+        {
+            CurrentLevel = 1;
+            CurrentStage++;
+
+            if (CurrentStage == 8)
+            {
+                CurrentStage = 1;
+                GoToMainMenu();
+            }
+        }
+
+        string levelName = "Level" + CurrentStage.ToString() + CurrentLevel.ToString();
+        PlayerPrefs.SetInt(levelName, 1);
+    }
+
+    public LevelData GetLevel() //lấy dữ liệu level hiện tại
+    {
+        string levelName = "Level" + CurrentStage.ToString() + CurrentLevel.ToString();
+        // Nếu là level 1, ưu tiên lấy đúng file LevelX1
+        if (CurrentLevel == 1)
+        {
+            if (Levels.ContainsKey(levelName))
+            {
+                return Levels[levelName];
+            }
+        }
+        // Các level khác giữ nguyên
+        if (Levels.ContainsKey(levelName))
+        {
+            return Levels[levelName];
+        }
+        return DefaultLevel;
+    }
+
+    public void GoToMainMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(MainMenu);
+    }
+
+    public void GoToGameplay()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(Gameplay);
+    }
+}
--- Assets/_Project/Scripts/LevelButton.cs
+++ Assets/_Project/Scripts/LevelButton.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class LevelButton : MonoBehaviour
+public class LevelButton : NMonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Color _inactiveColor;
+    [SerializeField] private Button _button;
+    [SerializeField] TMP_Text _levelText;
+    [SerializeField] private Image _image;
+
+    private bool isLevelUnlocked;
+    private int currentLevel;
+
+    protected override void Awake()
     {
-        
+        _button.onClick.AddListener(Clicked);
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnEnable()
     {
-        
+        MainMenuManager.Instance.LevelOpened += LevelOpened;
     }
-}
+
+    protected override void OnDisable()
+    {
+        MainMenuManager.Instance.LevelOpened -= LevelOpened;
+    }
+
+    protected void LevelOpened()
+    {
+        string gameObjectName = gameObject.name;
+        string[] parts = gameObjectName.Split('_');
+        _levelText.text = parts[parts.Length - 1];
+        currentLevel = int.Parse(_levelText.text);
+        isLevelUnlocked = GameManager.Instance.IsLevelUnlocked(currentLevel);
+
+        if (isLevelUnlocked)
+        {
+            _image.color = MainMenuManager.Instance.CurrentColor;
+        }
+        else
+        {
+            _image.color = _inactiveColor;
+        }
+    }
+
+    public void Clicked()
+    {
+        if (!isLevelUnlocked)
+            return;
+
+        GameManager.Instance.CurrentLevel = currentLevel;
+        GameManager.Instance.GoToGameplay();
+    }
